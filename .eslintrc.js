bd6a96caf5104d6c775c646140b72e9da16baa2c
Chu Hyeonjin <choo51455145@gmail.com>
2021-01-31 13:19:06 +0900

    🔧 | 린터 및 번들러 설정파일 추가

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    node: true,
+  },
+  rules: {
+    // enable additional rules
+    indent: ['error', 2],
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/app/index.tsx',
+  output: {
+    filename: '[name]-bundle.js',
+    path: resolve(__dirname, 'build'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader?sourceMap',
+          },
+          {
+            loader: 'less-loader?sourceMap',
+          },
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
